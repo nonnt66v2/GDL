4325c85f2d4b4899c0b6a67e4ce9c2bae2b91e5f
nonnt66v2 <91503962+nonnt66v2@users.noreply.github.com>
2024-03-06 16:36:34 +0100

    aggiunta codice - screen lezione

---
+++ esercizi/2)hello-world/main.c
@@ -0,0 +1,30 @@
+//
+// Created by nonnt66 on 06/03/24.
+//
+
+//libreria, insieme di funzioni e metodi
+#include <stdio.h>
+
+// insieme di operazioni che verranno eseguite alla compilazione e all'esecuzione del programma
+int main() {
+    //stampa a video
+
+    //tipo //nome variabile = //informazione;
+//    int marco = 2;
+//    //%d = prendi il valore decimale ovvero intero dalla variabile che ti do
+//    printf("%d",marco);
+
+    //stampare a video l'iniziale e l'età di un ragazzo
+    //inizializzazione
+    int anni, penne;
+    char iniziale;
+    //assegnazione
+    penne = 3;
+    anni = 22;
+    iniziale = 'p';
+    printf("%d %d %c", iniziale, anni, iniziale);
+
+
+}
+
+
