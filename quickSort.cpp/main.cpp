09b3647c27903dd77755bfcffa0f55a1ee3e7127
nonnt66v2 <lorenzo.dipalo001@studenti.uniparthenope.it>
2025-02-11 15:59:18 +0100

    quickSOrt

--- quickSort.cpp/main.cpp
+++ quickSort.cpp/main.cpp
@@ -1,6 +1,8 @@
 // C++ Program to demonstrate how to implement the quick
 // sort algorithm
 #include <bits/stdc++.h>
+#include <cstring>
+
 using namespace std;
 
 int partition(vector<int> &vec, int low, int high) {
@@ -8,7 +10,7 @@ int partition(vector<int> &vec, int low, int high) {
     // Selecting last element as the pivot
     int pivot = vec[high];
 
-    // Index of elemment just before the last element
+    // Index of element just before the last element
     // It is used for swapping
     int i = (low - 1);
 
@@ -29,7 +31,7 @@ int partition(vector<int> &vec, int low, int high) {
     return (i + 1);
 }
 
-void quickSort(vector<int> &vec, int low, int high) {
+void quickSort(vector<int> &vec, int low, int high) { // NOLINT(*-no-recursion)
 
     // Base case: This part will be executed till the starting
     // index low is lesser than the ending index high
@@ -47,16 +49,49 @@ void quickSort(vector<int> &vec, int low, int high) {
 }
 
 int main() {
-    vector<int> vec = {0, 10, 12, 3, 4, 6};
-    int n = vec.size();
 
-    // Calling quicksort for the vector vec
-    quickSort(vec, 0, n - 1);
+    std::ifstream file("IN.txt"); // Apri il file input.txt
+    std::ofstream f("OUT.txt"); // Apri il file input.txt
 
-    for (auto i : vec) {
-        cout << i << ", ";
+
+    if (!file) {
+        std::cerr << "Errore apertura file!" << std::endl;
+        return 1; // Ritorna un valore di errore
     }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        size_t pos = line.find("car: [");
+        size_t fine = line.find("]},");
+        string car = line.substr(pos + 6, fine - pos - 6);
+        stringstream ss(car);
+        string token;
+        vector<int> vec;
+        while (getline(ss, token, ',')) {
+            vec.push_back(stoi(token));
+        }
+
+        quickSort(vec, 0, (int) vec.size() - 1);
+        stringstream a;
+        for (auto x: vec) {
+            a << x << ",";
+        }
+        line.replace(pos + 6, fine - pos - 6, a.str());
+        size_t virgola = line.find(",]");
+        line.replace(virgola, 2, "]");
+
+        cout << line << endl;
+        f << line << endl;
+        cout << endl;
+
+    }
+
+    file.close();
+    f.close();// Chiudi il file
+
+
     return 0;
 }
+
 
 
